e547c0a8c65f1e4e743da71b1b8e7fa6b27b01ac
Miro Kähärä <miro23005@student.hamk.fi>
2024-01-29 14:34:32 +0200

    Testi

--- nappula/nappula/MainWindow.xaml.cs
+++ nappula/nappula/MainWindow.xaml.cs
@@ -25,5 +25,10 @@ namespace nappula
         {
           
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
